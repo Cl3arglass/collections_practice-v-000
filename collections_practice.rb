41cf9311ca58f8cb0e517d28a71f4a972ee6fe9e
Holden Altaffer <haltaffer@gmail.com>
2018-02-24 08:03:49 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -19,5 +19,5 @@ def reverse_array(array)
 end
 
 def kesha_maker(array)
-  array.map{|element| element[0,2] + "$" + element[4, -1]}
+  array.map{|element| element[0..2] + "$" + element[4..-1]}
 end
